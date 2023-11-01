23fd795482d3b216c2f4f26dd20081efd29ca5b1
Abhishek-vamja <abhishekvamja2518@gmail.com>
2023-11-01 11:18:48 +0530

    add template and views.

--- FruitShop/user/urls.py
+++ FruitShop/user/urls.py
@@ -5,18 +5,34 @@ from django.urls import path
 from user.views import *
 
 urlpatterns = [
-    path('' , login_attempt , name="login"),
-    path('register/' , register , name="register"),
-    path('otp/' , otp , name="otp"),
-    path('login-otp/', login_otp , name="login_otp"),
-    path('login-email/', login_email, name='login-email'),
-    path('logout/', user_logout, name='logout'),
+    # USER AUTHENTICATION...
+    path('' , UserAuth.login_attempt , name="login"),
+    path('register/' , UserAuth.register , name="register"),
+    path('otp/' , UserAuth.otp , name="otp"),
+    path('login-otp/', UserAuth.login_otp , name="login_otp"),
+    path('login-email/', UserAuth.login_email, name='login-email'),
+    path('logout/', UserAuth.user_logout, name='logout'),
 
 
-    path('your-account/', user_account, name='user-account'),
-    path('your-address/', user_address, name='user-address'),
-    path('new-address/', add_user_address, name='new-address'),
-    path('make-default/<uuid:address_id>/', make_default_address, name='make-default'),
-    path('remove-address/<uuid:address_id>/', remove_user_address, name='remove-address')
+    # USER ACCOUNT...
+    path('your-account/', UserAccounts.user_account, name='user-account'),
     
+    # USER ACCOUNT >> USER ORDERS...
+    # Make Soon..
+
+
+    # USER ACCOUNT >> LOGIN & SECURITY...
+    # Make Soon..
+
+
+    # USER ACCOUNT >> USER ADDRESS...
+    path('your-address/', UserAccounts.UserAddress.user_address, name='user-address'),
+    path('new-address/', UserAccounts.UserAddress.add_user_address, name='new-address'),
+    path('change-address/<uuid:address_id>/', UserAccounts.UserAddress.user_change_address, name='change-address'),
+    path('make-default/<uuid:address_id>/', UserAccounts.UserAddress.make_default_address, name='make-default'),
+    path('remove-address/<uuid:address_id>/', UserAccounts.UserAddress.remove_user_address, name='remove-address')
+    
+
+    # USER ACCOUNT >> CONTACT...
+    # Make Soon..
 ]
--- FruitShop/user/views.py
+++ FruitShop/user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
 from django.contrib.auth import logout, login, authenticate
 from django.views.generic import View
@@ -12,206 +12,248 @@ import http.client
 from django.views.decorators.csrf import csrf_exempt
 
 
-def send_otp(mobile , otp):
-    """
-    Send otp to user mobile.
-    """
-    print("FUNCTION CALLED")
-    conn = http.client.HTTPSConnection("api.msg91.com")
-    authkey = settings.AUTH_KEY 
-    headers = {
-    "accept": "application/json",
-    "content-type": "application/json",
-    "authkey": authkey
-    }
-    url = "https://control.msg91.com/api/v5/otp?mobile=91"+mobile
-    conn.request("GET", url , headers=headers)
-    res = conn.getresponse()
-    data = res.read()
-    print(data)
-    return None
+class UserAuth:
+    """Handle user authentication"""
+    def send_otp(mobile , otp):
+        """
+        Send otp to user mobile.
+        """
+        print("FUNCTION CALLED")
+        conn = http.client.HTTPSConnection("api.msg91.com")
+        authkey = settings.AUTH_KEY 
+        headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authkey": authkey
+        }
+        url = "https://control.msg91.com/api/v5/otp?mobile=91"+mobile
+        conn.request("GET", url , headers=headers)
+        res = conn.getresponse()
+        data = res.read()
+        print(data)
+        return None
 
 
-def user_logout(request):
-    """Logout user."""
-    logout(request)
-    return redirect('/shop/')
+    def user_logout(request):
+        """Logout user."""
+        logout(request)
+        return redirect('/shop/')
 
 
-def login_attempt(request):
-    if request.method == 'POST':
-        mobile = request.POST.get('mobile')
+    def login_attempt(request):
+        if request.method == 'POST':
+            mobile = request.POST.get('mobile')
+            
+            user = Profile.objects.filter(mobile=mobile).first()
+            
+            if user is None:
+                context = {'message' : 'User not found' , 'class' : 'danger' }
+                return render(request,'user/login.html' , context)
+            
+            otp = str(random.randint(1000 , 9999))
+            user.otp = otp
+            user.save()
+            send_otp(mobile , otp)
+            request.session['mobile'] = mobile
+            return redirect('login_otp')        
+        return render(request,'user/login.html')
+
+
+    @csrf_exempt
+    def login_otp(request):
+        mobile = request.session['mobile']
+        context = {'mobile':mobile}
+        if request.method == 'POST':
+            otp = request.POST.get('otp')
+            profile = Profile.objects.filter(mobile=mobile).first()
+            
+            if otp == profile.otp:
+                user = User.objects.get(id = profile.user.id)
+                login(request , user)
+                return redirect('index')
+            else:
+                context = {'message' : 'Wrong OTP' , 'class' : 'danger','mobile':mobile }
+                return render(request,'user/verify_otp.html' , context)
         
-        user = Profile.objects.filter(mobile=mobile).first()
-        
-        if user is None:
-            context = {'message' : 'User not found' , 'class' : 'danger' }
-            return render(request,'user/login.html' , context)
-        
-        otp = str(random.randint(1000 , 9999))
-        user.otp = otp
-        user.save()
-        send_otp(mobile , otp)
-        request.session['mobile'] = mobile
-        return redirect('login_otp')        
-    return render(request,'user/login.html')
+        return render(request,'user/verify_otp.html' , context)
 
-@csrf_exempt
-def login_otp(request):
-    mobile = request.session['mobile']
-    context = {'mobile':mobile}
-    if request.method == 'POST':
-        otp = request.POST.get('otp')
-        profile = Profile.objects.filter(mobile=mobile).first()
-        
-        if otp == profile.otp:
-            user = User.objects.get(id = profile.user.id)
-            login(request , user)
-            return redirect('index')
-        else:
-            context = {'message' : 'Wrong OTP' , 'class' : 'danger','mobile':mobile }
-            return render(request,'user/verify_otp.html' , context)
+
+    # Not working properly..
+    def login_email(request):
+        """Login with email and password."""
+        if request.method == "POST":
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            user = authenticate(request,username=email, password=password)
+            print(user)
+
+            if user is not None:
+                print('login')
+                login(request, user)
+                return redirect('index')
+            else:
+                print('In Elseeee')
+                context = {
+                    'message': 'email and password are not valid',
+                    'class': 'danger',
+                }
+                return render(request, 'user/login_email.html', context)
+            
+        return render(request, 'user/login_email.html')
+
+
+    def register(request):
+        if request.method == 'POST':
+            email = request.POST.get('email')
+            name = request.POST.get('name')
+            password = request.POST.get('password')
+            mobile = request.POST.get('mobile')
+            
+            check_user = User.objects.filter(email = email).first()
+            check_profile = Profile.objects.filter(mobile = mobile).first()
+            
+            if check_user or check_profile:
+                context = {'message' : 'User already exists' , 'class' : 'danger' }
+                return render(request,'user/register.html' , context)
+                
+            user = User(email = email , name = name, password = password)
+            user.save()
+            otp = str(random.randint(1000 , 9999))
+            profile = Profile(user = user , mobile=mobile , otp = otp) 
+            profile.save()
+            send_otp(mobile, otp)
+            request.session['mobile'] = mobile
+            return redirect('otp')
+        return render(request,'user/register.html')
+
+    @csrf_exempt
+    def otp(request):
+        mobile = request.session['mobile']
+        context = {'mobile':mobile}
+        if request.method == 'POST':
+            otp = request.POST.get('otp')
+            profile = Profile.objects.filter(mobile=mobile).first()
+            
+            if otp == profile.otp:
+                user = User.objects.get(id = profile.user.id)
+                login(request , user)
+                return redirect('index')
+            else:
+                print('Wrong')
+                
+                context = {'message' : 'Wrong OTP' , 'class' : 'danger','mobile':mobile }
+                return render(request,'user/verify_otp.html' , context)
+                
+            
+        return render(request,'user/verify_otp.html' , context)
+
+
+"""
+User Profile session.
+"""
+
+class UserAccounts(LoginRequiredMixin):
+    """Handle user account."""
+    def user_account(request):
+        """User detail."""
+        greeting = get_time_of_day_greeting()
+
+        context = {
+            'greeting': greeting
+        }
+        return render(request, 'user/profile.html', context)
     
-    return render(request,'user/verify_otp.html' , context)
-
-# Not working properly..
-def login_email(request):
-    """Login with email and password."""
-    if request.method == "POST":
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user = authenticate(request,username=email, password=password)
-        print(user)
-
-        if user is not None:
-            print('login')
-            login(request, user)
-            return redirect('index')
-        else:
-            print('In Elseeee')
+    class UserAddress:
+        """HAndle address of account."""
+        def user_address(request):
+            """Users Address."""
+            address = Address.objects.filter(user=request.user)
             context = {
-                'message': 'email and password are not valid',
-                'class': 'danger',
+                'address': address
             }
-            return render(request, 'user/login_email.html', context)
-        
-    return render(request, 'user/login_email.html')
+            return render(request, 'user/user_address.html', context)
 
 
-def register(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        name = request.POST.get('name')
-        password = request.POST.get('password')
-        mobile = request.POST.get('mobile')
-        
-        check_user = User.objects.filter(email = email).first()
-        check_profile = Profile.objects.filter(mobile = mobile).first()
-        
-        if check_user or check_profile:
-            context = {'message' : 'User already exists' , 'class' : 'danger' }
-            return render(request,'user/register.html' , context)
+        def add_user_address(request):
+            """Add new address for user."""
+            if request.method == "POST":
+                country = request.POST.get('country')
+                full_name = request.POST.get('full_name')
+                mobile = request.POST.get('mobile')
+                pincode = request.POST.get('pincode')
+                flat =  request.POST.get('flat')
+                area =  request.POST.get('area')
+                landmark = request.POST.get('landmark')
+                city =  request.POST.get('city')
+                state = request.POST.get('state')
+                user = request.user
+
+                address = Address(
+                    user=user, full_name=full_name, mobile=mobile,
+                    pincode=pincode, flat=flat, area=area,
+                    landmark=landmark, city=city, state=state, country=country
+                )
+                address.save()
+                messages.success(request, 'Address added successfully!!')
             
-        user = User(email = email , name = name, password = password)
-        user.save()
-        otp = str(random.randint(1000 , 9999))
-        profile = Profile(user = user , mobile=mobile , otp = otp) 
-        profile.save()
-        send_otp(mobile, otp)
-        request.session['mobile'] = mobile
-        return redirect('otp')
-    return render(request,'user/register.html')
+            return render(request, 'user/user_add_address.html', locals())
 
-@csrf_exempt
-def otp(request):
-    mobile = request.session['mobile']
-    context = {'mobile':mobile}
-    if request.method == 'POST':
-        otp = request.POST.get('otp')
-        profile = Profile.objects.filter(mobile=mobile).first()
-        
-        if otp == profile.otp:
-            user = User.objects.get(id = profile.user.id)
-            login(request , user)
-            return redirect('index')
-        else:
-            print('Wrong')
+
+        def user_change_address(request,address_id):
+            """Change address as needed."""
+            address = get_object_or_404(Address, id=address_id)
+
+            if request.method == 'POST':
+                country = request.POST.get('country')
+                full_name = request.POST.get('full_name')
+                mobile = request.POST.get('mobile')
+                pincode = request.POST.get('pincode')
+                flat =  request.POST.get('flat')
+                area =  request.POST.get('area')
+                landmark = request.POST.get('landmark')
+                city =  request.POST.get('city')
+                state = request.POST.get('state')
+                user = request.user
+
+                """
+                Update the fields of the address object.
+                """
+                Address.objects.filter(id=address_id).update(
+                    country=country,full_name=full_name,mobile=mobile,pincode=pincode,
+                    flat=flat,area=area,landmark=landmark,city=city,state=state,user=user)
+                messages.success(request, 'Address change successfully!!')
+                return redirect('user-address')
             
-            context = {'message' : 'Wrong OTP' , 'class' : 'danger','mobile':mobile }
-            return render(request,'user/verify_otp.html' , context)
-            
-        
-    return render(request,'user/verify_otp.html' , context)
+            context = {
+                'obj': address
+            }
+            return render(request, 'user/user_change_address.html', context)
 
 
-"""User Profile session."""
+        def make_default_address(request,address_id):
+            """Make default address"""
+            try:
+                address_to_set_default = Address.objects.get(id=address_id)
+                address_to_set_default.default = True
+                address_to_set_default.save()
 
-def user_account(request):
-    """User detail."""
-    greeting = get_time_of_day_greeting()
-
-    context = {
-        'greeting': greeting
-    }
-    return render(request, 'user/profile.html', context)
-
-
-def user_address(request):
-    """Users Address."""
-    address = Address.objects.filter(user=request.user)
-    context = {
-        'address': address
-    }
-    return render(request, 'user/user_address.html', context)
+                """
+                Set all other addresses as non-default.
+                """
+                Address.objects.exclude(id=address_id).update(default=False)
+                messages.success(request, 'Changed default address successfully!!')
+                return redirect('user-address')
+            except Address.DoesNotExist:
+                
+                print('EEEEEEEE')    
 
 
-def add_user_address(request):
-    """Add new address for user."""
-    if request.method == "POST":
-        country = request.POST.get('country')
-        full_name = request.POST.get('full_name')
-        mobile = request.POST.get('mobile')
-        pincode = request.POST.get('pincode')
-        flat =  request.POST.get('flat')
-        area =  request.POST.get('area')
-        landmark = request.POST.get('landmark')
-        city =  request.POST.get('city')
-        state = request.POST.get('state')
-        user = request.user
-
-        address = Address(
-            user=user, full_name=full_name, mobile=mobile,
-            pincode=pincode, flat=flat, area=area,
-            landmark=landmark, city=city, state=state, country=country
-        )
-        address.save()
-        messages.success(request, 'Address added successfully!!')
-    
-    return render(request, 'user/user_add_address.html', locals())
-
-def make_default_address(request,address_id):
-    """Make default address"""
-    try:
-        address_to_set_default = Address.objects.get(id=address_id)
-        address_to_set_default.default = True
-        address_to_set_default.save()
-
-        """
-        Set all other addresses as non-default.
-        """
-        Address.objects.exclude(id=address_id).update(default=False)
-        messages.success(request, 'Changed default address successfully!!')
-        return redirect('user-address')
-    except Address.DoesNotExist:
-        
-        print('EEEEEEEE')    
-
-def remove_user_address(request, address_id):
-    """Remove user address."""
-    address = Address.objects.get(id=address_id)
-    address.delete()
-    messages.success(request ,'Remove address successfully!!')
-    return redirect('user-address')
+        def remove_user_address(request, address_id):
+            """Remove user address."""
+            address = Address.objects.get(id=address_id)
+            address.delete()
+            messages.success(request ,'Remove address successfully!!')
+            return redirect('user-address')
 
 
 def get_time_of_day_greeting():
